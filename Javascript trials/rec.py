4f276de3c3355cb9c88c0ab4766708a074d23f3b
pammikoundi <pammikoundi@outlook.com>
2023-11-18 14:50:11 -0500

    push

--- Javascript trials/rec.py
+++ Javascript trials/rec.py
@@ -1,61 +1,63 @@
-# app.py
-
 from flask import Flask, request, jsonify
 from lightfm import LightFM
-from lightfm.datasets import fetch_movielens
-import numpy as np
+from lightfm.data import Dataset
 
 app = Flask(__name__)
 
-# Load the MovieLens dataset
-data = fetch_movielens()
+# Placeholder for the trained model
+model = None
+dataset = None
+user_dict = None
+item_dict = None
 
-# Train the LightFM model
-model = LightFM(loss='warp')
-model.fit(data['train'], epochs=30, num_threads=2)
+@app.route('/train', methods=['POST'])
+def train_model():
+    global model, dataset, user_dict, item_dict
 
-# Store user feedback (dummy data for demonstration purposes)
-user_feedback = {}
+    # Assume the data is sent as JSON with the structure {"interactions": [...], "user_features": [...], "item_features": [...]}
+    data = request.get_json()
 
-@app.route('/recommend', methods=['POST'])
-def recommend():
-    try:
-        user_id = int(request.json['user_id'])
-        n_recommendations = int(request.json['n_recommendations'])
-    except KeyError:
-        return jsonify({"error": "Invalid request. 'user_id' and 'n_recommendations' are required."}), 400
+    # Prepare dataset and model as before
+    dataset = Dataset()
+    dataset.fit(users=user_dict.values(), items=item_dict.values())
 
-    # Get recommendations for the user
-    recommendations = model.predict(user_id, range(data['item_features'].shape[0]))
+    interactions, _ = dataset.build_interactions(data['interactions'])
+    
+    # Assuming binary user and item features
+    user_features = dataset.build_user_features(data['user_features'], normalize=False)
+    item_features = dataset.build_item_features(data['item_features'], normalize=False)
 
-    # Sort recommendations by predicted scores
-    recommendations = sorted(zip(data['item_labels'], recommendations), key=lambda x: -x[1])[:n_recommendations]
+    model = LightFM(loss='warp')
+    model.fit(interactions, user_features=user_features, item_features=item_features, epochs=30, num_threads=2)
 
-    return jsonify({"user_id": user_id, "recommendations": recommendations})
+    return jsonify({"message": "Model trained successfully"})
 
-@app.route('/feedback', methods=['POST'])
-def feedback():
-    try:
-        user_id = int(request.json['user_id'])
-        item_id = int(request.json['item_id'])
-        feedback = int(request.json['feedback'])  # Assume 1 for like, -1 for dislike
-    except KeyError:
-        return jsonify({"error": "Invalid request. 'user_id', 'item_id', and 'feedback' are required."}), 400
+@app.route('/predict/<int:user_id>', methods=['GET'])
+def predict(user_id):
+    global model, user_dict, item_dict
 
-    # Update the user feedback
-    user_feedback.setdefault(user_id, {})
-    user_feedback[user_id][item_id] = feedback
+    if model is None:
+        return jsonify({"error": "Model not trained yet"})
 
-    # Create a new matrix including the user feedback
-    feedback_matrix = np.zeros_like(data['train'].toarray())
-    for user, feedback_dict in user_feedback.items():
-        for item, f in feedback_dict.items():
-            feedback_matrix[user - 1, item - 1] = f
+    user_id_to_predict = user_dict[user_id]
+    item_ids = range(len(item_dict))
 
-    # Retrain the model with the updated matrix
-    model.fit(feedback_matrix, epochs=30, num_threads=2)
+    # Assume binary user and item features
+    #Users are entities for whom recommendations are being generated.
+    #Items are entities that are being recommended to users.
+    user_features = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]  # Replace with actual binary user features
+    item_features = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]  # Replace with actual binary item features
 
-    return jsonify({"success": True})
+    # Predict scores for the items
+    scores = model.predict(user_id_to_predict, item_ids, user_features=user_features, item_features=item_features)
+
+    # Rank items based on scores
+    ranked_items = sorted(zip(item_ids, scores), key=lambda x: -x[1])
+
+    # Return the top N recommendations (let's say top 5)
+    top_recommendations = [{"item_id": item_id, "score": score} for item_id, score in ranked_items[:5]]
+
+    return jsonify({"user_id": user_id, "recommendations": top_recommendations})
 
 if __name__ == '__main__':
     app.run(debug=True)

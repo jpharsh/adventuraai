05f5d138b9020f557ff7ba94ac422471bf2a8ad8
jpharsh <jp.harsha@outlook.com>
2023-11-17 22:12:35 -0500

    Update with nav bar

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 const questions = [
-    "Are you ready?",
-    "Do you like traveling?",
-    "Have you ever been to Europe?",
-    "Do you enjoy trying new foods?",
-    "Is coding fun for you?",
-    "Have you ever climbed a mountain?",
-    "Do you want to experience different foods?",
-  ];
+  "Are you ready?",
+  "Do you like traveling?",
+  "Have you ever been to Europe?",
+  "Do you enjoy trying new foods?",
+  "Is coding fun for you?",
+  "Have you ever climbed a mountain?",
+  "Do you want to experience different foods?",
+];
+
+let currentQuestionIndex = 0;
+
+function answerQuestion(answer) {
+  // Process the answer if needed (not implemented in this example)
   
-  let currentQuestionIndex = 0;
-  
-  function answerQuestion(answer) {
-    // Process the answer if needed (not implemented in this example)
-    
-    // Display the next question
-    displayNextQuestion();
+  // Display the next question
+  displayNextQuestion();
+}
+
+function displayNextQuestion() {
+  if (currentQuestionIndex < questions.length - 1) {
+    currentQuestionIndex++;
+    document.getElementById('question-text').innerText = questions[currentQuestionIndex];
+  } else {
+    // End of questions
+    document.getElementById('question-container').innerHTML = '<p>Thank you for answering all the questions!</p>';
   }
-  
-  function displayNextQuestion() {
-    if (currentQuestionIndex < questions.length - 1) {
-      currentQuestionIndex++;
-      document.getElementById('question-text').innerText = questions[currentQuestionIndex];
-    } else {
-      // End of questions
-      document.getElementById('question-container').innerHTML = '<p>Thank you for answering all the questions!</p>';
-    }
-  }
-  
+}

9261cb27506d390501a2c1d00d44cd77b82b3df0
pammikoundi <pammikoundi@outlook.com>
2023-11-18 07:32:32 -0500

    update readme push fixes

--- gmaps_api.js
+++ gmaps_api.js
@@ -1,22 +1,39 @@
-        // Replace 'YOUR_API_KEY' with your actual Google API key
         const apiKey = 'AIzaSyA2mvBYUCSwer6A31nvCq54EmbCP_kscbY';
+
+        const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 
         // Function to make a request to the Google Places API
         function getPlaces(type, locationData) {
-            const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${locationData.lat},${locationData.lng}&radius=5000&type=${type}&key=${apiKey}`;
-
-            fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data); // Log the response data to the console
-                displayResults(data.results.slice(0, 5), type);
-            })
-            .catch(error => console.error('Error fetching data:', error));
+            const url = `${proxyUrl}https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${locationData.lat},${locationData.lng}&radius=5000&type=${type}&key=${apiKey}`;
         
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data); // Log the response data to the console
+                    displayResults(data.results.slice(0, 5), type);
+                })
+                .catch(error => console.error('Error fetching data:', error));
         }
+
+        // Function to get coordinates for a city
+function getCoordinatesForCity(cityName) {
+    const geocodingUrl = `${proxyUrl}https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(cityName)}&key=${apiKey}`;
+
+    return fetch(geocodingUrl)
+        .then(response => response.json())
+        .then(data => {
+            const location = data.results[0].geometry.location;
+            return { lat: location.lat, lng: location.lng };
+        })
+        .catch(error => {
+            console.error('Error fetching coordinates:', error);
+            return null;
+        });
+}
+
         // Function to display results on the webpage
         function displayResults(results, type) {
-            const resultsDiv = document.getElementById('resultsDiv');
+            const resultsDiv = document.getElementById('trial');
             resultsDiv.innerHTML += `<h2>${type}</h2><ul>`;
 
             results.forEach(place => {
@@ -27,9 +44,21 @@
         }
 
         // Example: Get top 5 transportation maps, hotels, attractions, and food around a specific location
-        const myLocation = { lat: 41.8781, lng: -87.6298 }; // Chicago coordinates
 
-        getPlaces('transit_station', myLocation);
-        getPlaces('lodging', myLocation); // Hotels
-        getPlaces('tourist_attraction', myLocation); // Attractions
-        getPlaces('restaurant', myLocation); // Food
+        const cityName = 'Atlanta'; // Replace with the desired city name
+        getCoordinatesForCity(cityName)
+            .then(coordinates => {
+                if (coordinates) {
+                    getPlaces('transit_station', coordinates);
+                    getPlaces('lodging', coordinates); // Hotels
+                    getPlaces('tourist_attraction', coordinates); // Attractions
+                    getPlaces('restaurant', coordinates); // Food
+                }
+            });
+        
+        
+        
+        
+        
+        
+        
